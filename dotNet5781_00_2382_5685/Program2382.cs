68e505b64d6a843ba4ea00c73fc69d71dba958e0
achinoamons <achinoamons@gmail.com>
2020-10-22 17:12:23 +0300

    i did 2 function

---
+++ dotNet5781_00_2382_5685/Program2382.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_2382_5685
+{
+   partial class Program2382
+    {
+        static void Main(string[] args)
+
+        {
+            Welcome2382();
+            Welcome5685();
+            Console.ReadKey();
+        }
+
+        private static void Welcome2382()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+        static partial void Welcome5685();
+        
+    }
+}
