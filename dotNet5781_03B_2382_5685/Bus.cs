75856b1ed40aa869d48ffa67e723bbac130a297c
תהילה זר <tilushzer20@gmail.com>
2020-12-03 15:45:12 +0200

    חחחחיח

---
+++ dotNet5781_03B_2382_5685/Bus.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_03B_2382_5685
+{
+    /// <summary>
+    /// class of bus that describe a bus
+    /// </summary>
+    class Bus
+    {
+        string NumBus;
+        DateTime StartDate;//Date of commencement of activity  Of the means of transport
+        DateTime LastDate;//the last date of taking care of the bus
+        double Kilometrath;
+        double Fuel;
+        double KilometrathAfterTipul;//km after treatment
+
+        public string ProNumBus//the property of NumBus
+        {
+            set
+            { NumBus = value; }
+            get { return NumBus; }
+        }
+        public DateTime ProStartDate//the property of StartDate
+        {
+            set
+            { StartDate = value; }
+            get { return StartDate; }
+        }
+        public DateTime ProLastDate//the property of LastDate
+        {
+            set
+            { LastDate = value; }
+            get { return LastDate; }
+        }
+        public double ProKilometrath//the property of Kilometrath
+        {
+            set
+            { Kilometrath = value; }
+            get { return Kilometrath; }
+        }
+        public double ProFuel//the property of Fuel
+        {
+            set
+            { Fuel = value; }
+            get { return Fuel; }
+        }
+        public double ProKilometrathAfterTipul//the property of Fuel
+        {
+            set
+            { KilometrathAfterTipul = value; }
+            get { return KilometrathAfterTipul; }
+        }
+        static Random r = new Random(DateTime.Now.Millisecond);
+        public static bool AddBus(List<Bus> L1, string num, int D, ref DateTime date)//מוסיפה אוטובוס
+        {
+            //int counter = 0;
+            bool isEmpty = !L1.Any();
+            if (!isEmpty)
+            {
+                foreach (Bus bus in L1)
+                {
+                    if (bus.ProNumBus == num)//check if there is already a bus like this
+                    {
+                        Console.WriteLine("the bus is already exist-insert again");
+                        return false;
+
+                    }
+
+
+                }
+            }
+
+            if ((date.Year < 2018 && D == 7) || (date.Year >= 2018 && D == 8))//Input integrity checker
+            {
+                return true;
+            }
+
+            else
+                Console.WriteLine("wrong input-try again");
+            return false;
+
+
+
+        }
+        public static void ChooseBusForTravel(List<Bus> L1, string num)
+        {
+            bool isEmpty = !L1.Any();
+
+            if (!isEmpty)
+            {
+                foreach (Bus bus in L1)
+                {
+                    if (bus.ProNumBus == num)//check if there is already a bus like this
+                    {
+                        DateTime currentDate = DateTime.Now;
+                        //// Only if less than a year has passed since the previous treatment
+                        if ((bus.ProLastDate.Year == currentDate.Year) || ((bus.ProLastDate.Year + 1 == currentDate.Year) && (12 - bus.ProLastDate.Month + currentDate.Month <= 12)))
+                        {
+
+                            double g = r.Next(1200);
+
+                            if (bus.ProKilometrathAfterTipul + g <= 20000)
+                            {
+                                bus.ProKilometrath += g;//
+                                bus.ProKilometrathAfterTipul += g;//uptade the relevant fields
+                                Console.WriteLine("success-uptading the relevant fields");
+                            }
+                            else
+                                Console.WriteLine("The bus cannot make the trip");
+
+                        }
+                        else
+                            Console.WriteLine("The bus cannot make the trip");//because more than a year has pass since the last treatment
+
+                        break;
+                    }
+                    else
+                        Console.WriteLine("The bus does not exist in the system");
+
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("The bus does not exist in the system");
+                //return false;
+            }
+
+        }
+        public static void FuelOrCare(List<Bus> L1, string num, int n)
+        {
+            bool isEmpty = !L1.Any();
+            if (!isEmpty)
+            {
+                foreach (Bus bus in L1)
+                {
+                    if (bus.ProNumBus == num)//check if there is already a bus like this
+                    {
+                        if (n == 1)//if is delek
+                        {
+                            bus.ProFuel += 1200;//update the fuel more a 1200
+                            Console.WriteLine("The bus got fuel successfully");
+                            return;
+                        }
+                        else
+                        {
+                            DateTime currentDate = DateTime.Now;
+                            bus.ProLastDate = currentDate;//updet tha last day that was tipul
+                            bus.ProKilometrathAfterTipul = 0;
+                            Console.WriteLine("The bus was handled successfully");
+                            return;
+                        }
+                    }
+                }
+                //Console.WriteLine("The bus does not exist in the system");//if the bus is not exist
+
+            }
+
+            Console.WriteLine("The bus does not exist in the system");//if the bus is not exist
+        }
+        public static void ShowKmSinceLastTreatment(List<Bus> L1)
+        {
+            bool isEmpty = !L1.Any();
+            if (!isEmpty)
+            {
+                foreach (Bus bus in L1)//Presentation of the km since the last treatment for all vehicles in the company.
+                {
+                    Console.Write("Registration Number:");
+                    if (bus.ProStartDate.Year < 2018)//print 7 digits
+                    {
+
+                        Console.WriteLine("{0}-{1}-{2}", bus.ProNumBus.Substring(0, 2), bus.ProNumBus.Substring(2, 3), bus.ProNumBus.Substring(5, 2));
+                    }
+                    else//print 8 digits
+                    {
+                        Console.WriteLine("{0}-{1}-{2}", bus.ProNumBus.Substring(0, 3), bus.ProNumBus.Substring(3, 2), bus.ProNumBus.Substring(5, 3));
+
+                    }
+                    Console.WriteLine("Km since last treatment:{0}", bus.ProKilometrathAfterTipul);
+
+                }
+            }
+        }
+
+    }
+}
--- dotNet5781_03B_2382_5685/MainWindow.xaml.cs
+++ dotNet5781_03B_2382_5685/MainWindow.xaml.cs
@@ -20,8 +20,61 @@ namespace dotNet5781_03B_2382_5685
     /// </summary>
     public partial class MainWindow : Window
     {
+        static Random r = new Random(DateTime.Now.Millisecond);
+
         public MainWindow()
         {
+            List<Bus> L1 = new List<Bus>();
+            DateTime d;
+            for (int i = 0; i < 10; i++)
+            {
+                string s;
+                /*int num= r.Next(1000000, 99999999);//randomaly number of licence
+               
+                string s = num.ToString();
+                if (s.Length==7)//7 DIGITS
+                {
+                     d = new DateTime(r.Next(31), r.Next(13), r.Next(2018));//random for date
+                }
+                else if(s.Length==8)//8 digits
+                {
+                    d = new DateTime(r.Next(31), r.Next(13), r.Next(2022));
+                }*/
+                d = new DateTime(r.Next(31), r.Next(13), r.Next(1970, 2022));//random for date
+                if (d.Year < 2018)
+                {
+                    int num = r.Next(1000000, 10000000);
+                    s = num.ToString();
+
+                }
+                else
+                {
+                    int num = r.Next(10000000, 100000000);
+                    s = num.ToString();
+                }
+
+                if (Bus.AddBus(L1, s, s.Length, ref d))//if its ok to add
+                {
+                    Bus B = new Bus();
+
+                    B.ProNumBus = s;
+                    B.ProStartDate = d;
+                    B.ProLastDate = d;//Update that last treatment date is the start date of activity
+                    L1.Add(B);
+                    //Console.WriteLine("succeed");
+                }//אם תהיה בעיה---לטפל בזה---לגבי רנדומליזציה---
+            }
+            //taking care of other demands
+
+            //L1[0].ProLastDate = DateTime.Now.Day;
+            L1[1].ProKilometrathAfterTipul = 1180;//ITS A SIGN THAT ITS NEED FUAL
+            L1[2].ProFuel = 10;//ITS A SIGN THAT ITS NEED FUAL
+            L1[3].ProFuel=20;//its a sign that its need tipul
+            L1[4].ProKilometrathAfterTipul = 1850;//its a sign that its need tipul
+
+
+
+
             InitializeComponent();
         }
     }
