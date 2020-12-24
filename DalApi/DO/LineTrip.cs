4869d912fbdd7f560353a2b66181bf48fc14699b
תהילה זר <tilushzer20@gmail.com>
2020-12-24 11:03:55 +0200

    i did some clases at dl

--- DalApi/DO/LineTrip.cs
+++ DalApi/DO/LineTrip.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DalApi.DO
+namespace DO
 {
-    class LineTrip
+    public class LineTrip
     {
+        public int LineTripId { get; set; }
+        public int LineId { get; set; }
+        public TimeSpan Frequency { get; set; }
+        public TimeSpan StartAt { get; set; }
+        public TimeSpan FinishAt { get; set; }
+
     }
 }
--- DalApi/DO/Trip.cs
+++ DalApi/DO/Trip.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DalApi.DO
+namespace DO
 {
-    class Trip
+   public class Trip
     {
+        public int IdTrip { get; set; }
+        public string UserName { get; set; }
+        public int LineId { get; set; }
+        public int InStation { get; set; }
+        public TimeSpan InAt { get; set; }
+        public int OutStation { get; set; }
+        public TimeSpan OutAt { get; set; }
+
     }
 }
