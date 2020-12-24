d233ebcf89f4dc6d9754f8f83cd0283fe29b0459
תהילה זר <tilushzer20@gmail.com>
2020-12-24 23:25:02 +0200

    i did class linestation in bo

--- BL/BO/LineStation.cs
+++ BL/BO/LineStation.cs
@@ -8,5 +8,17 @@ namespace BO
 {
     public class LineStation
     {
+        IEnumerable<BO.Line> ListOfLines;
+        public int CodeStation { get; set; }//מספר תחנה
+        public string Name { get; set; }//שם תחנה
+        public int LineStationIndex { get; set; }//מס תחנה בקו---- כלומר איזה מס תחנה אני בקו
+        //שיכלולים
+        public int PrevStationCode { get; set; }//תחנה קודמת
+        public int NextStationCode { get; set; }//תחנה הבאה
+        public double DistancePrevStation { get; set; }//מרחק מתחנה קודמת
+        public double DistanceNextStation { get; set; }//מרחק מתחנה הבאה
+        public TimeSpan TimePrevStation { get; set; }//זמן מתחנה קודמת
+        public TimeSpan TimeNextStation { get; set; }//זמן מתחנה הבאה
+
     }
 }
