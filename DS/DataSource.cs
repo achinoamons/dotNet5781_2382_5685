47764cf8a6fce9281201ea64c28d46aca08a503a
achinoamons <achinoamons@gmail.com>
2020-12-24 12:28:46 +0200

    we added for bonus

---
+++ DS/DataSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+
+namespace DS
+{
+    public  static class DataSource
+    {
+        public static List<Line> ListLines;
+        public static List<LineStation> ListLineStations;
+        public static List<LineTrip> ListLineTrips;
+        public static List<Station> ListStations;
+        public static List<Trip> ListTrips;
+        public static List<AdjacentStations> ListAdjacentStations;
+
+        static DataSource()
+        {
+            InitAllLists();
+        }
+        static  void InitAllLists()
+        {
+
+
+        }
+    }
+}
