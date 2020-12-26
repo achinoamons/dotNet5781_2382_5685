78506ead81b912f950901ccb4effcefe5a415530
תהילה זר <tilushzer20@gmail.com>
2020-12-27 01:24:23 +0200

    i add details to createstation

--- DS/CreateStations.cs
+++ DS/CreateStations.cs
@@ -40,33 +40,33 @@ namespace DS
             st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
             st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
             ///////////////
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
+            st.Add(new Station() { CodeStation = 38863, Name = "ההרחבה ב", Latitude = 32.300264, Longitude = 34.939512 });
+            st.Add(new Station() { CodeStation = 38864, Name = "ההרחבה/הותיקים", Latitude = 32.298171, Longitude = 34.939512 });
+            st.Add(new Station() { CodeStation = 38865, Name = "רשות שדות התעופה/העליה", Latitude = 31.990876, Longitude = 34.8976 });
+            st.Add(new Station() { CodeStation = 38866, Name = "כנף/ברוש", Latitude = 31.998767, Longitude = 34.879725 });
+            st.Add(new Station() { CodeStation = 38867, Name = "החבורה/דב הוז", Latitude = 31.883019, Longitude = 34.818708 });
 
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
+            st.Add(new Station() { CodeStation = 38869, Name = "בית הלוי ה", Latitude = 32.349776, Longitude = 34.926837 });
+            st.Add(new Station() { CodeStation = 38870, Name = "הראשונים/כביש 5700", Latitude = 32.352953, Longitude = 34.899465 });
+            st.Add(new Station() { CodeStation = 38872, Name = "הגאון בן איש חי/צאלון", Latitude = 31.897286, Longitude = 34.775083 });
+            st.Add(new Station() { CodeStation = 38873, Name = "עוקשי/לוי אשכול", Latitude = 31.883941, Longitude = 34.807039 });
+            st.Add(new Station() { CodeStation = 38875, Name = "מנוחה ונחלה/יהודה גורודיסקי", Latitude = 31.896762, Longitude = 34.816752 });
+            st.Add(new Station() { CodeStation = 38876, Name = "גורודסקי/יחיאל פלדי", Latitude = 31.898463, Longitude = 34.823461 });
+            st.Add(new Station() { CodeStation = 38877, Name = "דרך מנחם בגין/יעקב חזן", Latitude = 32.076535, Longitude = 34.904907 });
+            st.Add(new Station() { CodeStation = 38878, Name = "דרך הפארק/הרב נריה", Latitude = 32.299994, Longitude = 34.878765 });
+            st.Add(new Station() { CodeStation = 38879, Name = "התאנה/הגפן", Latitude = 31.865457, Longitude = 34.859437 });
+            st.Add(new Station() { CodeStation = 3880, Name = "התאנה/האלון", Latitude = 31.866772, Longitude = 34.864555 });
 
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
-            st.Add(new Station() { CodeStation = 38831, Name = "בי\"ס בר לב / בן יהודה", Latitude = 32.183921, Longitude = 34.917806 });
+            st.Add(new Station() { CodeStation = 3881, Name = "דרך הפרחים/יסמין", Latitude = 31.809325, Longitude = 31.809325 });
+            st.Add(new Station() { CodeStation = 3883, Name = "יצחק רבין/פנחס ספיר", Latitude = 31.80037, Longitude = 34.778239 });
+            st.Add(new Station() { CodeStation = 3884, Name = "מנחם בגין/יצחק רבין", Latitude = 31.799224, Longitude = 34.782985 });
+            st.Add(new Station() { CodeStation = 3885, Name = "חיים הרצוג/דולב", Latitude = 31.800334, Longitude = 34.785069 });
+            st.Add(new Station() { CodeStation = 3886, Name = "בית ספר גוונים/ארז", Latitude = 31.802319, Longitude = 34.786735 });
+            st.Add(new Station() { CodeStation = 3887, Name = "דרך האילנות/אלון", Latitude = 31.804595, Longitude = 34.786623 });
+            st.Add(new Station() { CodeStation = 3888, Name = "דרך האילנות/מנחם בגין", Latitude = 31.805041, Longitude = 34.785098 });
+            st.Add(new Station() { CodeStation = 3889, Name = "העצמאות/וייצמן", Latitude = 31.816751, Longitude = 34.782252 });
+            st.Add(new Station() { CodeStation = 3890, Name = "וייצמן/מרבד הקסמים", Latitude = 31.816579, Longitude = 34.779753 });
+            st.Add(new Station() { CodeStation = 3891, Name = "צאלה/אלמוג", Latitude = 31.801182, Longitude = 31.801182 });
 
         }
     }
