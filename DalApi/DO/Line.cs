7635683dfddb20813d5533dbaf3320298ad569f0
תהילה זר <tilushzer20@gmail.com>
2021-01-07 12:27:05 +0200

    aba

--- DalApi/DO/Line.cs
+++ DalApi/DO/Line.cs
@@ -8,9 +8,13 @@ namespace DO
 {
     public class Line
     {
+
+
+
+
         public int LineID { get; set; }//קןד רץ יחודי---ספציפי לדוג קו 5
         public int Code { get; set; }//קוד ספציפי לדו קו 5 ירושלים וקו 5 ת"א---אז לכל 1 יהיה קוד שונה
-        public  Areas Area { get; set; }//אזור שבו הקו פעיל
+        public Areas Area { get; set; }//אזור שבו הקו פעיל
         public int FirstStation { get; set; }
         public int LastStation { get; set; }
         public override string ToString() => this.ToStringProperty();
